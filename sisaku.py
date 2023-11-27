87f91d3fcd3e788df3b6f3e10806e3989acb7856
川上 大心 <109069759+daithin@users.noreply.github.com>
2023-11-27 16:00:33 +0900

    windows上で動く、センサ未実装　仮コード
    windows上では求める機能は実装できた。ラズパイ向けにカスタマイズする。

--- sisaku.py
+++ sisaku.py
@@ -1,17 +1,15 @@
 from google.cloud import vision
 import io
 import pyttsx3
+import cv2  # OpenCVライブラリ
 
 def speak(engine, text):
     engine.say(text)
     engine.runAndWait()
 
-def detect_objects(path):
-    """Local image fileで物体検出を行います。"""
+def detect_objects(content):
+    """画像データから物体を検出します。"""
     client = vision.ImageAnnotatorClient()
-
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
 
     image = vision.Image(content=content)
     response = client.object_localization(image=image)
@@ -19,30 +17,43 @@ def detect_objects(path):
 
     result = []
     for object_ in objects:
-        result.append((object_.name, object_.score))
+        result.append(object_.name)
 
     if response.error.message:
         raise Exception('{}\nFor more info on error messages, check: https://cloud.google.com/apis/design/errors'.format(response.error.message))
     
     return result
 
-def image_recognition(image_path):
-    """画像データから物体を認識して結果を返します。"""
-    recognized_objects = detect_objects(image_path)
-    return recognized_objects
+def capture_image():
+    """カメラを使用して画像をキャプチャします。"""
+    cap = cv2.VideoCapture(0)  # 0はデフォルトのカメラを指します
+
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        return None
+    cap.release()
+
+    # OpenCVの画像をJPEG形式のバイトデータに変換
+    ret, buffer = cv2.imencode('.jpg', frame)
+    if not ret:
+        print("Failed to encode image")
+        return None
+
+    return buffer.tobytes()
 
 def sensor_and_camera():
     detected = True  # センサーが物体を検出したか
     long = 5  # 物体までの距離
-    image_data = r"C:\Users\81802\Desktop\画像自分フォト\ookami.png"  # カメラの画像データ
+    image_data = capture_image()  # カメラの画像データ
     return detected, long, image_data
 
 if __name__ == "__main__":
     engine = pyttsx3.init()  # 音声エンジンを初期化
 
     detected, long, image_data = sensor_and_camera()
-    if detected:
+    if detected and image_data is not None:
         speak(engine, f"{long}メートル")
-        recognized_objects = image_recognition(image_data)
-        for name, score in recognized_objects:
-            speak(engine, f"{name} detected with confidence {score}")
+        recognized_objects = detect_objects(image_data)
+        for name in recognized_objects:
+            speak(engine, name)
